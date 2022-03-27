fa8071a76d69555f80aaed59bc2299c41912c6bb
altisseront <altisseront@gmail.com>
2022-03-26 22:28:34 -0400

    udpates

--- buttonclicker.js
+++ buttonclicker.js
@@ -8,4 +8,25 @@ function login(element) {
     else if (element.innerText == "Logout"){
         element.innerText = "Login";
     }
+
+
+}
+
+function addLike1() {
+    var x = document.getElementById('likes1');
+    var value1 = x.innerHTML;
+
+    ++value1;
+
+    console.log(value1);
+    document.getElementById('likes1').innerHTML = value1;
+}
+function addLike2() {
+    var x = document.getElementById('likes2');
+    var value2 = x.innerHTML;
+
+    ++value2;
+
+    console.log(value2);
+    document.getElementById('likes2').innerHTML = value2;
 }
